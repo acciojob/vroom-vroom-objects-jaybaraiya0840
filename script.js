a141927858ddf127a3a12b93fce0f7f1ded9f291
jaybaraiya0840 <jaybaraiya0802@gmail.com>
2025-03-14 19:49:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,36 @@ function SportsCar(make, model, topSpeed) {
 SportsCar.prototype= Object.create(Car.prototype);
 SportsCar.prototype.getTopSpeed=function () {
 	return this.topSpeed;	
+}
+// // create class car-> add properties to it and its methods
 
+// class Car {
+// 	constructor(make, model) {
+// 		this.make=make;
+// 		this.model=model;
+// 	}
+// }
+
+// Car.prototype.getMakeModel = function () {
+// 	return `${this.make} ${this.model}`
+// }
+
+// let obj={
+// 	make:2000,
+	
+// }
+// // create class sportsCar-> inherit car->create properties and its methods
+
+// class SportsCar {
+// 	constructor(make, model, topSpeed) {
+// 		super(make, model);
+// 		this.topSpeed=model;
+// 	}
+// }
+
+// SportsCarCar.prototype.getTopSpeed = function () {
+// 	return this.topSpeed;
+// }
 
 // Do not change the code below
 window.Car = Car;
